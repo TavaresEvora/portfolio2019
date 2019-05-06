244f855d38dda655501a070f05147769fa358ca9
Valdimir Tavares Evora <valdimir.tavaresevora@fidesio.com>
2019-05-06 16:59:25 +0200

    feat(about): Ajout du vrai texte

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 import SEO from '../components/seo'
+import variables from '../components/elements/variables';
 
 const StyledTitle = styled.h1`
+  width: 50%;
+  font-size: 1.1em;
+  font-weight: 500;
+  text-transform: uppercase;
+  margin: 0;
+`
 
+const StyledSubTitle = styled.h3`
+  font-size: 0.8em;
+  font-weight: 500;
+  text-transform: uppercase;
+  color: ${ variables.primary };
+  margin: 0;
 `
 
 const StyledContent = styled.div`
@@ -15,10 +28,53 @@ const StyledContent = styled.div`
   height: 100vh;
 `
 
-const StyledDescription = styled.p`
+const StyledSection = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
   max-width: 80%;
+  width: 100%;
+  padding: 70px 0;
+  &:first-child {
+    margin-top: 100vh;
+    padding-top: 0;
+  }
+  &:last-child {
+    padding-bottom: 0;
+  }
+`
+
+const StyledSectionContent = styled.div`
+  width: 50%;
+  /* margin: 0 70px; */
+`
+
+const StyledSectionSkill = styled.div`
+  margin: 30px 0;
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`
+
+const StyledDescription = styled.p`
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 50px;
+  }
   line-height: 1.8;
   margin-bottom: 25px;
+  .hl {
+    color: ${ variables.primary };
+  }
 `
 
 class AboutPage extends Component {
@@ -26,19 +82,53 @@ class AboutPage extends Component {
     return (
       <StyledContent>
         <SEO title="A propos" />
-          <StyledTitle>A Propos de moi</StyledTitle>
-          <StyledDescription>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          uas, eos veniam nobis nihil ut nisi! Harum, architecto?
-          Dignissimos consequuntur vitae repellat officiis ab cum.
-          Error voluptas esse aspernatur voluptatum nostrum?
-          </StyledDescription>
-          <StyledDescription>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          uas, eos veniam nobis nihil ut nisi! Harum, architecto?
-          Dignissimos consequuntur vitae repellat officiis ab cum.
-          Error voluptas esse aspernatur voluptatum nostrum?
-          </StyledDescription>
+          <StyledSection>
+            <StyledTitle>A Propos de moi</StyledTitle>
+            <StyledSectionContent>
+              <StyledDescription>Je m'appelle Tavares Evora Valdimir. Je suis Web Développeur.
+              Pendant mes formations j'ai appris le développement <span className="hl">back-end</span>,
+              puis étant passionné par le développement plus largement j'ai appris le développement
+              <span className="hl">front-end </span> ainsi que l'intégration.
+              </StyledDescription>
+              <StyledDescription>
+              Aujourd'hui je suis capable de m'occuper de la conception d'un site internet en passant par 
+              l'integration responsive, le développement <span className="hl">back-end </span>
+              et <span className="hl">front-end</span>.
+              </StyledDescription>
+              <StyledDescription>
+              J'accorde beaucoup d'importance à la qualité et l'optimisation du code produit.
+              </StyledDescription>
+              <StyledDescription>
+              A l'avenir, je souhaite en apprendre plus sur l'aspect DevOps
+              </StyledDescription>
+            </StyledSectionContent>
+          </StyledSection>
+
+          <StyledSection>
+            <StyledTitle>Mes compétences</StyledTitle>
+            <StyledSectionContent>
+              <StyledSectionSkill>
+                <StyledSubTitle>Templating</StyledSubTitle>
+                <StyledDescription>HTML / Twig / Blade</StyledDescription>
+              </StyledSectionSkill>
+              <StyledSectionSkill>
+                <StyledSubTitle>Javascript</StyledSubTitle>
+                <StyledDescription>Natif / ES6 / React / VueJs / JQuery / Gsap / NodeJs / Websocket</StyledDescription>
+              </StyledSectionSkill>
+              <StyledSectionSkill>
+                <StyledSubTitle>CSS</StyledSubTitle>
+                <StyledDescription>Sass / Responsive / Bootstrap / BEM</StyledDescription>
+              </StyledSectionSkill>
+              <StyledSectionSkill>
+                <StyledSubTitle>PHP</StyledSubTitle>
+                <StyledDescription>Natif / Laravel / Symfony / Wordpress</StyledDescription>
+              </StyledSectionSkill>
+              <StyledSectionSkill>
+                <StyledSubTitle>Environnement</StyledSubTitle>
+                <StyledDescription>Git / Npm / Yarn / Webpack / Visual Studio Code / PhpStorm</StyledDescription>
+              </StyledSectionSkill>
+            </StyledSectionContent>
+          </StyledSection>
       </StyledContent>
     )
   }
