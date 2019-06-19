ecf469543b786baa04d5d3e623481756b32b979c
Valdimir Tavares Evora <valdimir.tavaresevora@fidesio.com>
2019-06-19 16:05:40 +0200

    style(about): page about terminé

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { TimelineMax } from 'gsap'
 import ScrollMagic from 'scrollmagic'
-import { TimelineLite } from 'gsap'
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators'
 
 import SEO from '../components/seo'
 import variables from '../components/elements/variables';
@@ -29,8 +31,14 @@ const StyledTitle = styled.h2`
   font-weight: 500;
   text-transform: uppercase;
   margin: 50px 0;
+  overflow: hidden;
   @media (min-width: 768px) {
     width: 50%;
+    margin: 0;
+  }
+
+  > span {
+    display: block;
     margin: 0;
   }
 `
@@ -38,6 +46,7 @@ const StyledTitle = styled.h2`
 const StyledSubTitle = styled.h3`
   font-size: 0.8em;
   font-weight: 500;
+  overflow: hidden;
   text-transform: uppercase;
   color: ${ variables.primary };
   margin: 0;
@@ -52,7 +61,9 @@ const StyledContent = styled.div`
 
 const StyledSection = styled.div`
   display: flex;
+  margin: 0 auto;
   justify-content: center;
+  align-items: baseline;
   flex-wrap: wrap;
   max-width: 80%;
   width: 100%;
@@ -64,6 +75,9 @@ const StyledSection = styled.div`
 
 const StyledFirstSection = styled(StyledSection)`
   padding-top: 0;
+  @media (min-width: 768px) {
+    padding: 0;
+  }
 `
 
 const StyledSectionContent = styled.div`
@@ -90,7 +104,8 @@ const StyledSectionSkill = styled.div`
   }
 `
 
-const StyledDescription = styled.p`
+const StyledDescription = styled.div`
+  overflow: hidden;
   &:first-child {
     margin-top: 0;
   }
@@ -104,83 +119,123 @@ const StyledDescription = styled.p`
   line-height: 1.8;
   margin-bottom: 25px;
   .hl {
-    color: ${ variables.primary };
+    color: ${ variables.divrimary };
+  }
+
+  > p {
+    display: block;
+    margin: 0;
   }
 `
 
 class AboutPage extends Component {
   constructor(props) {
     super(props)
-    this.tl = new TimelineLite({onUpdate: this.updatePercentage, paused: true})
+    this.updatePercentage = this.updatePercentage.bind(this)
+    this.tl = new TimelineMax({onUpdate: this.updatePercentage})
     this.controller = new ScrollMagic.Controller()
-    this.scene = new ScrollMagic.Scene()
-  }
-
-  componentDidMount() {
-    // this.controller
-    this.tl.from('.name', 1.5, { opacity: 0 })
-      .play()
-
-    this.scene({
+    this.scene = new ScrollMagic.Scene({
       triggerElement: '.sections',
       triggerHook: 'onLeave',
       duration: '100%'
     })
+  }
+
+  componentDidMount() {
+    // this.controller
+    this.tl
+      .to('.name', 1.5, { x: '-100%' })
+
+    this.scene
       .setPin('.sections')
       .setTween(this.tl)
       .addTo(this.controller)
+
+    document.querySelectorAll('.section').forEach((el) => {
+      const title = el.querySelector('.title span')
+      const tlm = new TimelineMax()
+      tlm.from(title, .5, { y: '100%' })
+      new ScrollMagic.Scene({
+        triggerElement: el,
+        triggerHook: 0.8,
+        reverse: false
+      })
+        .setTween(tlm)
+        .addTo(this.controller)
+
+      el.querySelectorAll('.text').forEach((text) => {
+        const textT = text.querySelector('p')
+        const tlmt = new TimelineMax()
+        tlmt.from(textT, .5, { y: '100%' })
+        new ScrollMagic.Scene({
+          triggerElement: text,
+          triggerHook: 0.9,
+          reverse: false
+        })
+          .setTween(tlmt)
+          .addTo(this.controller)
+      })
+    })
+  }
+
+  updatePercentage() {
+    this.tl.progress()
+    // this.tlT.progress()
   }
 
   render() {
     return (
       <StyledContent>
         <SEO title="A propos" />
-          <StyledName><h1 className="name">Tavares Evora</h1></StyledName>
-          <div className="sections">
-            <StyledFirstSection>
-              <StyledTitle>A Propos de moi</StyledTitle>
+          <StyledName className="sections"><h1 className="name">Tavares Evora</h1></StyledName>
+          <div>
+            <StyledFirstSection className="section">
+              <StyledTitle className="title"><span>A Propos de moi</span></StyledTitle>
               <StyledSectionContent>
-                <StyledDescription>Je m'appelle Tavares Evora Valdimir. Je suis Web Développeur.
+                <StyledDescription className="text">
+                  <p>Je m'appelle Tavares Evora Valdimir. Je suis Web Développeur.
                 Pendant mes formations j'ai appris le développement <span className="hl">back-end</span>,
                 puis étant passionné par le développement plus largement j'ai appris le développement
-                <span className="hl"> front-end </span> ainsi que l'intégration.
+                <span className="hl"> front-end </span> ainsi que l'intégration.</p>
                 </StyledDescription>
-                <StyledDescription>
-                Aujourd'hui je suis capable de m'occuper de la conception d'un site internet en passant par 
-                l'integration responsive, le développement <span className="hl">back-end </span>
-                et <span className="hl">front-end</span>.
+                <StyledDescription className="text">
+                  <p>
+                    Aujourd'hui je suis capable de m'occuper de la conception d'un site internet en passant par 
+                    l'integration responsive, le développement <span className="hl">back-end </span>
+                    et <span className="hl">front-end</span>.
+                  </p>
                 </StyledDescription>
-                <StyledDescription>
-                J'accorde beaucoup d'importance à la qualité et l'optimisation du code produit.
+                <StyledDescription className="text">
+                  <p>J'accorde beaucoup d'importance à la qualité et l'optimisation du code produit.</p>
                 </StyledDescription>
-                <StyledDescription>
-                A l'avenir, je souhaite en apprendre plus sur l'aspect DevOps
+                <StyledDescription className="text">
+                  <p>A l'avenir, je souhaite en apprendre plus sur l'aspect DevOps</p>
                 </StyledDescription>
               </StyledSectionContent>
             </StyledFirstSection>
 
-            <StyledSection>
-              <StyledTitle>Mes compétences</StyledTitle>
+            <StyledSection className="section">
+              <StyledTitle className="title"><span>Mes compétences</span></StyledTitle>
               <StyledSectionContent>
                 <StyledSectionSkill>
-                  <StyledSubTitle>Templating</StyledSubTitle>
-                  <StyledDescription>HTML / Twig / Blade</StyledDescription>
+                  <StyledSubTitle className="text"><p>Templating</p></StyledSubTitle>
+                  <StyledDescription className="text"><p>HTML / Twig / Blade</p></StyledDescription>
                 </StyledSectionSkill>
                 <StyledSectionSkill>
-                  <StyledSubTitle>Javascript</StyledSubTitle>
-                  <StyledDescription>Natif / ES6 / React / VueJs / JQuery / Gsap / NodeJs / Websocket</StyledDescription>
+                  <StyledSubTitle className="text"><p>Javascript</p></StyledSubTitle>
+                  <StyledDescription className="text"><p>Natif / ES6 / React / VueJs / JQuery / Gsap / NodeJs / Websocket</p></StyledDescription>
                 </StyledSectionSkill>
                 <StyledSectionSkill>
-                  <StyledSubTitle>CSS</StyledSubTitle>
-                  <StyledDescription>Sass / Responsive / Bootstrap / BEM</StyledDescription>
+                  <StyledSubTitle className="text"><p>CSS</p></StyledSubTitle>
+                  <StyledDescription className="text"><p>Sass / Responsive / Bootstrap / BEM</p></StyledDescription>
                 </StyledSectionSkill>
                 <StyledSectionSkill>
-                  <StyledSubTitle>PHP</StyledSubTitle>
-                  <StyledDescription>Natif / Laravel / Symfony / Wordpress</StyledDescription>
+                  <StyledSubTitle className="text"><p>PHP</p></StyledSubTitle>
+                  <StyledDescription className="text"><p>Natif / Laravel / Symfony / Wordpress</p></StyledDescription>
                 </StyledSectionSkill>
                 <StyledSectionSkill>
-                  <StyledSubTitle>Environnement</StyledSubTitle>
-                  <StyledDescription>Git / Npm / Yarn / Webpack / Visual Studio Code / PhpStorm</StyledDescription>
+                  <StyledSubTitle className="text"><p>Environnement</p></StyledSubTitle>
+                  <StyledDescription className="text"><p>Git / Npm / Yarn / Webpack / Visual Studio Code / PhpStorm</p></StyledDescription>
                 </StyledSectionSkill>
               </StyledSectionContent>
             </StyledSection>
