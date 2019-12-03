890cb5a6b4d2ffd7d89031d99bc226109e8f1375
Valdimir Tavares Evora <valdimir.tavaresevora@fidesio.com>
2019-12-03 12:14:17 +0100

    style(*): ajout du favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
         background_color: '#01b1ec',
         theme_color: '#01b1ec',
         display: 'standalone',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
